5acd3ea9ac3d00919f9c8f71e71125401ab60249
wkylin <wkylin.w@gmail.com>
2020-02-29 14:38:46 +0800

    requestAnimationFrame

--- script/ring.js
+++ script/ring.js
@@ -45,3 +45,16 @@ function toggleBackfaces() {
     if (backfacesVisible) addClassName(shape, "backfaces");
     else removeClassName(shape, "backfaces");
 }
+window.requestAnimationFrame = window.requestAnimationFrame
+    || window.mozRequestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.msRequestAnimationFrame
+    || function(f){return setTimeout(f, 1000/60)}; // simulate calling code 60 
+
+window.cancelAnimationFrame = window.cancelAnimationFrame
+    || window.mozCancelAnimationFrame
+    || function(requestID){clearTimeout(requestID)};//fall ba
+
+setInterval(function() {
+    requestAnimationFrame(toggleShape);
+}, 16000)
